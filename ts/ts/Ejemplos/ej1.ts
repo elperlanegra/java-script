f6d660a937484460141b3337546bff1dd3201cc4
TheCodeMonster <carlosd99m@gmail.com>
2021-05-30 14:54:30 -0600

    ejemplo 1

---
+++ ts/ts/Ejemplos/ej1.ts
@@ -0,0 +1,26 @@
+
+// ejemplo usando JavaScript
+
+var nombre;
+nombre = "Miguelo";
+var edad;
+edad = 30;
+var PERSONAJE = {
+    nombre: nombre,
+    edad: edad
+};
+
+
+// Lo pasamos a typescript
+
+let nameUser: string
+nameUser = "Manuel Duarte"
+
+let age: number
+age = 21
+
+const Futbolista = {
+    nameUser: nameUser,
+    age: age
+}
+

38c44949b86d1dfc1959ed4cdd6f2e751f11de29
Manuel Duarte <carlosd99m@gmail.com>
2021-06-08 21:57:31 -0600

    datos primitivos js

--- js-moderno/javascript-basics-0.0.2/assets/js/primitivos.js
+++ js-moderno/javascript-basics-0.0.2/assets/js/primitivos.js
@@ -1,45 +1,55 @@
-
 // Tipos de datos primitivos
 
-let nombre = 'Carlos';  // String
-console.log( nombre )
+let nombre = "Carlos"; // String
+console.log(nombre);
 
+nombre = "Ben Parker";
+console.log(nombre);
 
-nombre = 'Ben Parker'
-console.log( nombre )
+nombre = "Tía May";
+nombre = "Tía Mayo";
 
-nombre = "Tía May"
-nombre = `Tía May`
+console.log(typeof nombre); // Para ver el tipo de dato que tiene
 
-console.log( typeof nombre ) // Para ver el tipo de dato que tiene
-
-nombre = 123
-console.log( nombre )
-
+nombre = 123;
+console.log(nombre);
 
 // Booleanos
 
-let esMarvel = true  //Boolean
-let esDC = false  // Se declara en minisculas
+let esMarvel = true; //Boolean
+let esDC = false; // Se declara en minisculas
 
-
-console.log( typeof esMarvel )
-
+console.log(typeof esMarvel);
 
 // Enteros
 
-edad = 33
-console.log( edad ) 
+edad = 33;
+console.log(edad);
 
-let superPoder // undefined
-console.log( typeof superPoder ) // la variable superPoder no tiene un valor inicializado
+let superPoder; // undefined
+console.log(typeof superPoder); // la variable superPoder no tiene un valor inicializado
 
-let soyNull = null
-console.log( typeof soyNull )
+let soyNull = null;
+console.log(typeof soyNull);
 
 // symbol
-let symbol1 = Symbol('a')
-let symbol2 = Symbol('b')
-console.log( typeof symbol1 )
+let symbol1 = Symbol("a");
+let symbol2 = Symbol("b");
+console.log(typeof symbol1);
 
-console.log ( symbol1 === symbol2 )
+console.log(symbol1 === symbol2);
+
+
+console.log("Datos primitivos")
+
+let arr = []
+
+const datos = {
+    name: "Manuel",
+    lastName: "Duarte",
+    age: 24,
+    direction: {
+        city: "Bilwi",
+        country: "Nicaragua"
+    }
+}

5b5ad2b7ea6a7dc359d0701405046358ca18ff8d
Manuel Duarte <carlosd99m@gmail.com>
2021-06-09 00:37:54 -0600

    :fire:

---
+++ js-moderno/javascript-basics-0.0.2/assets/js/arreglos.js
@@ -0,0 +1,20 @@
+// Arreglos
+
+// const arr = new Array(10);
+// console.log(arr);
+
+// const arr = [];
+
+let videoJuegos = ["Mario Cars", "Megaman", "Chrono Trigger"];
+// console.log(videoJuegos[0]); // Para mostrar la primera posicion
+// console.log(videoJuegos); // Imprime todo el arreglo
+
+let arregloCosas = [
+  true,
+  123,
+  "Manuel",
+  1 + 3,
+  ["X", "Megaman", "Zero", "Dr.Light"],
+];
+
+console.log(arregloCosas[4][3]);

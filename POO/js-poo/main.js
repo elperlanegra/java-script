aa0c93f505d48b1782b85eb87638e0109b646806
Manuel Duarte <carlosd99m@gmail.com>
2021-06-13 20:56:19 -0600

    buble-sort

---
+++ POO/js-poo/main.js
@@ -0,0 +1,13 @@
+var object = {
+    field1: 10,
+    field2: "text",
+    field3: function () { return this.field3; }
+};
+var paul = {
+    name: "paul",
+    getName: function () {
+        return this.name; //this es necesario!
+    }
+}
+
+console.log(paul)
